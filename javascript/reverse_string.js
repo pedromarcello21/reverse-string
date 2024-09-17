e3b6504008578ac213973b4e071252cec44036fe
pedro <vincentypedro@gmail.com>
2024-09-17 08:27:13 -0400

    solution

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,6 +1,12 @@
 function reverseString(str) {
   // type your code here
+  let reversed_str = '';
+  for(let i = str.length-1; i>=0; i--){
+    reversed_str += str[i]
+  }
+  return reversed_str
 }
+
 
 if (require.main === module) {
   // add your own tests in here
